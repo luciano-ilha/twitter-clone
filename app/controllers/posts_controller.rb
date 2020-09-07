a0998bffcbda2cc448e1552ae5160531f119fca4
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-09-07 17:52:34 -0300

    Fix view layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all.order("created_at DESC")
-    # @post = Post.new
+    @post = Post.new
   end
 
   # GET /posts/1
